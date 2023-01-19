96a82d76a167d50f33820ec508e4380fd6edabd6
Kitteh3 <heyheymamakat@gmail.com>
2023-01-19 17:57:54 -0500

    Add id's to numbers, event listeners on number buttons

--- javascript.js
+++ javascript.js
@@ -17,3 +17,11 @@ let operations = {
 function operate(numbers, operator) {
     return numbers.reduce(operations[operator]);
 };
+
+//Add event listeners to number buttons, put numbers in display area
+let numbers = Array.from(document.querySelectorAll('.number'));
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        console.log(number.id);
+    });
+});
