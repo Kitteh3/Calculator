79f5358a727f5970d918c8074f55e15c811646ba
Kitteh3 <heyheymamakat@gmail.com>
2023-01-19 15:47:22 -0500

    Add functions for basic math

---
+++ javascript.js
@@ -0,0 +1,13 @@
+//Basic mat functions
+const add = function(a,b) {
+    return a + b;
+};
+const subtract = function(a,b) {
+    return a - b;
+};
+const multiply = function(a,b) {
+    return a * b;
+};
+const divide = function(a,b) {
+    return a / b;
+};
