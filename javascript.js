1e1ff8d8c4fa47e224de19abe89760577f0a50af
Kitteh3 <heyheymamakat@gmail.com>
2023-01-20 14:22:20 -0500

    Create listners for operating buttons, fix string to numbers bug

--- javascript.js
+++ javascript.js
@@ -1,29 +1,38 @@
 //Basic math functions
 let operations = {
-    "+" : function(num1,num2) {
-        return num1 + num2;
+    "+" : function(a, b) {
+        a = Number(a);
+        b = Number(b);
+        return a + b;
     },
-    "-" : function(num1,num2) {
-        return num1 - num2;
+    "-" : function(a, b) {
+        return a - b;
     },
-    "*" : function(num1,num2) {
-        return num1 * num2;
+    "*" : function(a, b) {
+        return a * b;
     },
-    "/" : function(num1,num2) {
-        return num1 / num2;
+    "/" : function(a, b) {
+        return a / b;
     },
-}
+};
 //Operate function
 let operator = undefined;
 function operate(numbers, operator) {
     return numbers.reduce(operations[operator]);
 };
 
+//equals function
+/* let currentTotal = undefined;
+function findTotal(nums, operator) {
+    currentTotal = operate(nums, operator);
+} */
+
 //Add event listeners to number buttons, put numbers in display area
 let display = document.querySelector('.display');
 let displayContent = undefined;
 display.textContent = displayContent;
 const nums = [];
+let currentTotal = undefined;
 let numbers = Array.from(document.querySelectorAll('.number'));
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
@@ -43,15 +52,48 @@ equals.addEventListener('click', () => {
     nums.push(displayContent);
     display.textContent = operate(nums, operator);
     displayContent = display.textContent;
+    currentTotal = display.textContent;
     nums[0] = displayContent;
     nums.pop();
 });
 
  let dividedBy = document.querySelector('#divide');
  dividedBy.addEventListener('click', () => {
+    console.log(nums);
     nums.shift();
     nums.push(displayContent);
     operator = '/';
     display.textContent = undefined;
+    //if (nums.length === 2) {
+    //    currentTotal = operate(nums, operator);
+    //}
+    console.log(currentTotal);
+});
 
-})
+let multipliedBy = document.querySelector('#multiply');
+multipliedBy.addEventListener('click', () => {
+    console.log(nums);
+    nums.shift();
+    nums.push(displayContent);
+    operator = '*';
+    display.textContent = undefined;
+    console.log(currentTotal);
+});
+
+let minus = document.querySelector('#minus');
+minus.addEventListener('click', () => {
+    console.log(nums);
+    nums.shift();
+    nums.push(displayContent);
+    operator = '-';
+    display.textContent = undefined;
+    console.log(currentTotal);
+});
+
+let plus = document.querySelector('#plus');
+plus.addEventListener('click', () => {
+    nums.shift();
+    nums.push(displayContent);
+    operator = '+';
+    display.textContent = undefined;
+});
