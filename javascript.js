d5668ed24fab95e4688f09c071b8ec1b68c7c02d
Kitteh3 <heyheymamakat@gmail.com>
2023-01-20 11:05:12 -0500

    Store numbers in array, create divide event listener

--- javascript.js
+++ javascript.js
@@ -21,10 +21,16 @@ function operate(numbers, operator) {
 //Add event listeners to number buttons, put numbers in display area
 let display = document.querySelector('.display');
 let displayContent = 0;
+let currentValue = displayContent;
+const nums = [];
 let numbers = Array.from(document.querySelectorAll('.number'));
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
+        if (nums.length >= 1) {
+            display.textContent = undefined;
+        }
         displayContent = display.textContent += number.id;
+        currentValue = displayContent;
     });
 });
 
@@ -32,4 +38,12 @@ numbers.forEach((number) => {
 let clear = document.querySelector('#clear');
 clear.addEventListener('click', () => {
     display.textContent = undefined;
+    nums.length = 0;
+});
+ let dividedBy = document.querySelector('#divide');
+ dividedBy.addEventListener('click', () => {
+    nums.push(currentValue);
+    if (nums.length > 1) {
+        currentValue = operate(nums, '/');
+    }
 })
