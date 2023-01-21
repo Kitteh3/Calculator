86693a490d0af869bfec0735bc92a0d27edad8aa
Kitteh3 <heyheymamakat@gmail.com>
2023-01-21 14:46:02 -0500

    Add event listener to decimal button

--- javascript.js
+++ javascript.js
@@ -44,6 +44,18 @@ numbers.forEach((number) => {
     });
 });
 
+//Decimals, floating point numbers
+let decimal = document.querySelector('#decimal');
+decimal.addEventListener('click', () => {
+    if (nums.lenth === 0) {
+        nums[0] = 0;
+        display.textContent = 0;
+    };
+    displayContent = display.textContent += decimal.textContent;
+    //nums.push(displayContent);
+    console.log(nums);
+})
+
 //Add event listeners to operating buttons
 let clear = document.querySelector('#clear');
 clear.addEventListener('click', () => {
